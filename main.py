4a544f39bf6b48b54c8600a0f02f2fc29eaadc42
m.zhmachinsky <m.zhmachinsky@octaviangameart.ru>
2024-03-19 11:54:54 +0300

    [master] - add simple buttons event

--- main.py
+++ main.py
@@ -4,5 +4,22 @@ from PySide6.QtUiTools import QUiLoader
 loader = QUiLoader()
 app = QtWidgets.QApplication([])
 window = loader.load('./gui/gui.ui', None)
+
+
+line_edit = window.findChild(QtWidgets.QLineEdit, 'lineEdit')
+if line_edit is not None:
+    line_edit.setText('Hello, World!')
+else:
+    print("QLineEdit с именем 'lineEdit' не найден")
+
+
+def press_button():
+    window.findChild(QtWidgets.QLineEdit, 'lineEdit').setText('Button clicked')
+
+
+button = window.findChild(QtWidgets.QPushButton, 'pushButton_10')
+button.clicked.connect(press_button)
+
+
 window.show()
 app.exec()
